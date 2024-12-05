33f17816ce996d1058c1a1adc59c3cc767c1a2bc
phiewter <19486709+phiewter@users.noreply.github.com>
2024-12-05 23:43:31 +0100

    update: <Grid>

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -1,13 +1,26 @@
 import { Grid } from "@/components/ui/grid/grid";
+import React from "react";
 
 export default function Page() {
   return (
     <Grid.SystemContentWrapper>
       <Grid.System>
-        <Grid columns={3}>
-          <div className="bg-gray-700 text-white h-full w-full">1</div>
-          <div className="bg-gray-800 text-white h-full w-full">2</div>
-          <div className="bg-gray-900 text-white h-full w-full">3</div>
+        <Grid rows={4} columns={4}>
+          <Grid.Cell row="auto" column={1}>
+            Does
+          </Grid.Cell>
+          <Grid.Cell row="auto" column={4}>
+            this
+          </Grid.Cell>
+          <Grid.Cell row={2} column={3}>
+            grid
+          </Grid.Cell>
+          <Grid.Cell row={3} column={4}>
+            work
+          </Grid.Cell>
+          <Grid.Cell row={4} column="auto">
+            ?
+          </Grid.Cell>
         </Grid>
       </Grid.System>
     </Grid.SystemContentWrapper>
--- components/ui/grid/grid.tsx
+++ components/ui/grid/grid.tsx
@@ -4,11 +4,18 @@ import { cn } from "@/lib/utils";
 
 import styles from "@/components/ui/grid/grid.module.css";
 
-interface GridProps extends React.HTMLAttributes<HTMLDivElement> {
-  children?: React.ReactNode;
-  className?: string;
-  columns?: number;
-  rows?: number;
+// TODO: Fix 'ResponsiveBreakpoints' for Grid
+type ResponsiveBreakpoints = {
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+};
+
+interface GridProps {
+  rows: number;
+  columns: number;
+  children: React.ReactElement<GridCellProps>[];
 }
 
 const Grid: React.FC<GridProps> & {
@@ -16,17 +23,39 @@ const Grid: React.FC<GridProps> & {
   System: React.FC<GridSystemProps>;
   Cell: React.FC<GridCellProps>;
   Cross: React.FC<GridCrossProps>;
-} = ({ children, className, columns = 1, rows = 1, ...props }) => {
+  Crossline: React.FC<GridCrosslineProps>;
+  Guides: React.FC<GridGuidesProps>;
+  Guide: React.FC<GridGuideProps>;
+  Divider: React.FC<GridDividerProps>;
+} = ({ rows, columns, children }: GridProps) => {
   return (
     <div
-      className={cn("grid inset-0", className)}
-      style={{
-        gridTemplateColumns: `repeat(${columns}, 1fr)`,
-        gridTemplateRows: `repeat(${rows}, 1fr)`,
-      }}
-      {...props}
+      className={cn(`${styles.grid}`)}
+      style={
+        {
+          "--grid-rows": rows,
+          "--grid-columns": columns,
+        } as React.CSSProperties
+      }
     >
       {children}
+      <Grid.Guides>
+        {Array.from({ length: rows * columns }, (_, index) => {
+          const x = (index % columns) + 1;
+          const y = Math.floor(index / columns) + 1;
+          return (
+            <Grid.Guide
+              key={index}
+              style={
+                {
+                  "--x": `${x}`,
+                  "--y": `${y}`,
+                } as React.CSSProperties
+              }
+            />
+          );
+        })}
+      </Grid.Guides>
     </div>
   );
 };
@@ -46,17 +75,19 @@ Grid.SystemContentWrapper = ({ children, className }) => {
 };
 Grid.SystemContentWrapper.displayName = "GridSystemContentWrapper";
 
-interface GridSystemProps extends React.HTMLAttributes<HTMLDivElement> {
+interface GridSystemProps {
   children?: React.ReactNode;
+  guideWidth?: number;
 }
 
-Grid.System = ({ children, className }) => {
+Grid.System = ({ children, guideWidth = 1 }) => {
   return (
     <div
-      className={cn(`${styles.gridSystem}`, className)}
+      className={cn(`${styles.gridSystem}`)}
       style={
         {
           "--min-width": "250px",
+          "--guide-width": `${guideWidth}px`,
         } as React.CSSProperties
       }
     >
@@ -66,12 +97,26 @@ Grid.System = ({ children, className }) => {
 };
 Grid.System.displayName = "GridSystem";
 
-interface GridCellProps extends React.HTMLAttributes<HTMLDivElement> {
+interface GridCellProps {
+  row?: string | number;
+  column?: string | number;
   children?: React.ReactNode;
 }
 
-Grid.Cell = ({ children, className }) => {
-  return <div className={cn(className)}>{children}</div>;
+Grid.Cell = ({ row, column, children }: GridCellProps) => {
+  return (
+    <div
+      className={cn(`${styles.block}`)}
+      style={
+        {
+          "--grid-row": row,
+          "--grid-column": column,
+        } as React.CSSProperties
+      }
+    >
+      {children}
+    </div>
+  );
 };
 Grid.Cell.displayName = "GridCell";
 
@@ -80,8 +125,69 @@ interface GridCrossProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 Grid.Cross = ({ children, className }) => {
-  return <div className={cn(className)}>{children}</div>;
+  return (
+    <div
+      className={cn(`${styles.cross}`, className)}
+      data-grid-cross=""
+      style={{ "--cross-row": 1, "--cross-column": 1 } as React.CSSProperties}
+    >
+      {children}
+    </div>
+  );
 };
 Grid.Cross.displayName = "GridCross";
+
+interface GridCrosslineProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+Grid.Crossline = ({ children, className, style }) => {
+  return (
+    <div className={cn(`${styles.crossLine}`, className)} style={style}>
+      {children}
+    </div>
+  );
+};
+Grid.Crossline.displayName = "GridCrossline";
+
+interface GridGuidesProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+Grid.Guides = ({ children, className, style }) => {
+  return (
+    <div className={cn(`${styles.guides}`, className)} style={style}>
+      {children}
+    </div>
+  );
+};
+Grid.Guides.displayName = "GridGuides";
+
+interface GridGuideProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+Grid.Guide = ({ children, className, style }) => {
+  return (
+    <div
+      className={cn(
+        `${styles.guide} ${styles.xsGuide} ${styles.smGuide} ${styles.mdGuide} ${styles.lgGuide}`,
+        className
+      )}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+
+interface GridDividerProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+Grid.Divider = ({ children, className }) => {
+  return <div className={cn(`${styles.divider}`, className)}>{children}</div>;
+};
+Grid.Divider.displayName = "GridDivider";
 
 export { Grid };
